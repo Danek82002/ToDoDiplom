1575b691ebb415c31e4ab9100cf62616ed65fa24
Che <Che>
2022-04-29 10:30:09 +0300

    Initial Commit

---
+++ ToDoDiplom/ToDoDiplomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDiplomApp.swift
+//  ToDoDiplom
+//
+//  Created by Student on 29.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDiplomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
